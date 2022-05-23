978a3d676556f7934a92391fe3c1bbbb4b56aa9f
Juan Liboa <jpliboa@teco.com.ar>
2022-05-23 17:42:05 -0300

    agregando libreria

--- script.js
+++ script.js
@@ -92,4 +92,19 @@ inputTexto.addEventListener('change', () => {
 })
 
 
+/*let botonundefined =document.getElementById ("botonundefined")
 
+botonundefined.addEventListener("click" , () =>
+Toastify({
+    text: "Producto agregado al carrito",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+  )*/
